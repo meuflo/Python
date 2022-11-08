95a2fcb19d7e420d28e8e4d8a567b766f87a0314
meuflo <117744903+meuflo@users.noreply.github.com>
2022-11-08 16:19:24 +0100

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -1,19 +1,19 @@
-def encrypt(text,key):
+def encrypt(text,key): #Encryption function
     result = ""
     for i in range(len(text)):
         char=text[i]
-        result+= chr((ord(char) + key - 96) % 26 + 96)  #ord permet de convertir le mot en nombre (table ascii), chr fait l'inverse
-    return result                                       #ascii : a = 97 mais a = 0 donc on commence a 96 
+        result+= chr((ord(char) + key - 96) % 26 + 96)
+    return result                                        
 
-def decrypt(text,key):
+def decrypt(text,key): #Decryption function
     result = ""
     for i in range(len(text)):
         char=text[i]
-        result+= chr((ord(char) - ke    y - 96) % 26 + 96)
+        result+= chr((ord(char) - key - 96) % 26 + 96)
     return result
     
     
-def menu() : #Menu principal
+def menu() : #Menu function
     print("[1] Encrypt")
     print("[2] Decrypt")
     print("[0] Quitter")
@@ -22,13 +22,13 @@ menu()
 option = int(input("Choix : "))
 
 while option != 0 : 
-    if option == 1:
+    if option == 1: #Encryption
         text = input("Enter a word : ")
         key = int(input("Enter a key : "))
         print(encrypt(text,key))
         break
     
-    elif option == 2 : 
+    elif option == 2 : #Decryption
         text = input("Enter a word : ")
         key = int(input("Enter a key : "))
         print(decrypt(text,key))
