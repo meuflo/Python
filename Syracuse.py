a40d13ed4f2cfdfcc5cc063f20dfa09ddcb1021c
meuflo <117744903+meuflo@users.noreply.github.com>
2022-11-08 16:22:41 +0100

    Update Syracuse.py

--- Syracuse.py
+++ Syracuse.py
@@ -1,12 +1,12 @@
-def syracuse(Nb:int) -> list: #Fonction syracuse
+def syracuse(Nb:int) -> list: #Syracuse function
     Nb
     list = [] 
     while Nb != 1 :    
-        if (Nb % 2) == 0 :  #Si le chiffre est paire
+        if (Nb % 2) == 0 :  #If the number is even
             Nb = Nb // 2
             list.append(Nb)
 
-        else :   #Si le chiffre est impaire
+        else :   #If the number is odd
             Nb = Nb * 3 + 1
             list.append(Nb)
     return list
