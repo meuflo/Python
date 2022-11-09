abdbba29017879582675e16959bc5e5f0f076f60
meuflo <117744903+meuflo@users.noreply.github.com>
2022-11-09 11:14:47 +0100

    Update Moviequiz.py

--- Moviequiz.py
+++ Moviequiz.py
@@ -4,19 +4,19 @@ import os
 # years = []
 
 # for i in range(0,4):
-#     movies += [input("Saisir le titre d'un film : ")]
-#     years += [input(f"Saisir l'année de sortie de \"{movies[i]}\" : ")]
+#     movies += [input("Enter the title of a movie : ")]
+#     years += [input(f"Enter the year of exit of \"{movies[i]}\" : ")]
 
 movies = ["Titanic","LOL","Star wars VI","300","Avatar"]
 years = [1990,2008,1976,2010,2005]
 
-menu = ["Quitter","Afficher tous les films"] + [f"Afficher l'année de sortie du film {movie}" for movie in movies] + ["Lancer le Quizz"]
+menu = ["Leave","View all movies"] + [f"Show the year of release of the film {movie}" for movie in movies] + ["Launch the Quiz"]
 
 choice = 1
 while choice != 0:
     for (index,menuItem) in enumerate(menu):
         print(f"{index}. {menuItem}")
-    choice = int(input("Votre choix : "))
+    choice = int(input("Your choice : "))
     if choice == 1:
         os.system("cls")
         res = ""
@@ -28,20 +28,20 @@ while choice != 0:
         print("\n-------------------------------------------------------------")
         print(f"Le film {movies[choice-2]} est sorti en {years[choice-2]}.")
         print("-------------------------------------------------------------\n")
-    elif choice == len(menu) - 1: # Quizz
+    elif choice == len(menu) - 1: # Quiz
         score = 0
         movieIndexesKnown = []
         for i in range(0,len(movies)):
-            # Tirage d'un film au hasard
+            # Drawing of a random film
             randomMovieIndex = random.randint(0, len(movies) - 1)
             while randomMovieIndex in movieIndexesKnown:
                 randomMovieIndex = random.randint(0, len(movies) - 1)
             movieIndexesKnown += [randomMovieIndex]
 
-            # On stock la bonne réponse (la bonne année)
+            # We stock the right answer (the right year)
             goodAnswer = years[randomMovieIndex]
 
-            # On genère 3 choix d'année aléatoire
+            # We generate 3 random year choices
             randomYears = []
             for i in range(0,3):
                 randomYear = random.randint(1891,2022)
@@ -49,22 +49,22 @@ while choice != 0:
                     randomYear = random.randint(1891,2022)
                 randomYears += [randomYear]
 
-            # On ajoute ces 3 années au tableau de réponses possibles
+            # We add these 3 years to the table of possible answers
             choices = [goodAnswer] + randomYears
 
-            # On affiche la question et les choix
-            print(f"En quelle année est sortie le film {movies[randomMovieIndex]} ?")
+            # We display the question and the choices
+            print(f"In which year was the film released {movies[randomMovieIndex]} ?")
             letters = ["A","B","C","D"]
             random.shuffle(choices)
             for j in range(0, len(choices)):
                 print(f"{letters[j]}. {choices[j]}")
 
-            # On analyse la réponse
+            # We analyze the response
             answer = "Z"
             while answer not in letters:
-                answer = input("Votre réponse : ").upper()
+                answer = input("Your response : ").upper()
 
-            # On traite le score
+            # We process the score
             if choices[letters.index(answer)] == goodAnswer:
                 score += 1
         os.system("cls")
