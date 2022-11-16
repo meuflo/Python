b4c45c8226621cc7f0e48105076145c4895b45ff
meuflo <117744903+meuflo@users.noreply.github.com>
2022-11-16 11:21:15 +0100

    Update Sorting_by_selection.py

--- Sorting_by_selection.py
+++ Sorting_by_selection.py
@@ -1,16 +1,16 @@
 from random import *
 
-#tab = [35, 166.5, 13, 94, -7, 43.4, 70, 2] #Si on utilise pas la génération aléatoire
-tab = [randint(-100,500) for i in range(10)] #Génération aléatoire
+#tab = [35, 166.5, 13, 94, -7, 43.4, 70, 2] #If we do not use random generation
+tab = [randint(-100,500) for i in range(10)] #Random generation
 
-def sort_selection(tab) : #Trouver le nombre mini
+def sort_selection(tab) : #Find the minimum number
    for i in range(len(tab)) :     
        min = i
        for j in range(i+1, len(tab)) : 
            if tab[min] > tab[j] :
                min = j
                 
-       vp = tab[i] #Inverse la position des nombres dans la liste (valeur temporaire, vp = value to be processed)
+       vp = tab[i] #Invert the position of the numbers in the list
        tab[i] = tab[min]
        tab[min] = vp
 
@@ -19,5 +19,5 @@ def sort_selection(tab) : #Trouver le nombre mini
 sort_selection(tab)
  
 print ("The sorting table is : ")
-for i in range(len(tab)) : #Génération de la liste croissante
+for i in range(len(tab)) : #Generation of the growing list
     print(tab[i])
